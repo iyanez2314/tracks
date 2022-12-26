52e2ae47935eb683abb4c9db22d020e5e1d20e43
Isaac Yanez <isaac231467@gmail.com>
2022-12-26 15:45:50 -0600

    added a reusable form for the sign up and sign in to help us not have redundant code

--- src/screens/SignUpScreen.js
+++ src/screens/SignUpScreen.js
@@ -1,41 +1,27 @@
-import React, { useState, useContext } from "react";
+import React, { useContext } from "react";
 import { View, StyleSheet, TouchableOpacity } from "react-native";
-import { Text, Input, Button } from "react-native-elements";
+import { Text } from "react-native-elements";
 import Spacer from "../components/Spacer";
 import { Context as AuthContext } from "../context/authContext";
+import AuthForm from "../components/AuthForm";
 
 const SignUpScreen = ({ navigation }) => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
   const { state, signup } = useContext(AuthContext);
 
   return (
     <View style={styles.container}>
-      <Spacer>
-        <Text h3> Sign up for tracker</Text>
-      </Spacer>
-      <Input
-        label="Email"
-        value={email}
-        onChangeText={setEmail}
-        autoCapitalize="none"
-        autoCorrect={false}
+      <AuthForm
+        headerText="Sign up for tracker"
+        onSubmit={signup}
+        errorMessage={state.errorMessage}
+        submitButtonText="Sign up"
       />
-      <Spacer />
-      <Input
-        label="Password"
-        value={password}
-        onChangeText={setPassword}
-        autoCapitalize="none"
-        autoCorrect={false}
-        secureTextEntry
-      />
-      {state.errorMessage ? (
-        <Text style={styles.errorMessage}>{state.errorMessage}</Text>
-      ) : null}
-      <Spacer>
-        <Button title="Sign Up" onPress={() => signup({ email, password })} />
-      </Spacer>
+      <TouchableOpacity onPress={() => navigation.navigate("Signin")}>
+        <Spacer />
+        <Text style={styles.link}>
+          Already have an account? Sign in instead
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -52,11 +38,8 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     marginBottom: 200,
   },
-  errorMessage: {
-    fontSize: 16,
-    color: "red",
-    marginLeft: 15,
-    marginTop: 10,
+  link: {
+    color: "blue",
   },
 });
 export default SignUpScreen;
