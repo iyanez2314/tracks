9732d2c839cd5a768b1d69c233cef6380fa52daf
Isaac Yanez <isaac231467@gmail.com>
2022-12-25 19:58:29 -0600

    made some boiler plate code for the functionality for the account

--- src/context/authContext.js
+++ src/context/authContext.js
@@ -7,8 +7,30 @@ const authReducer = (state, action) => {
   }
 };
 
+const signup = (dispatch) => {
+  return ({ email, password }) => {
+    // make api request to sign up with that email and password
+    // if we sign up, modify our state, and say that we are authenticated
+    // if signing up fails we need to refelect that it failed
+  };
+};
+
+const signin = (dispatch) => {
+  return ({ email, password }) => {
+    // Try to sign in
+    // handle success by updating state
+    // handle faliure
+  };
+};
+
+const signout = (dispatch) => {
+  return () => {
+    // sign out
+  };
+};
+
 export const { Provider, Context } = createDataContext(
   authReducer,
-  {},
+  { signup, signin, signout },
   { isSignedIn: false }
 );
