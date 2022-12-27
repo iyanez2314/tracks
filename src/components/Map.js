aa957aacea96fc44c4fe6e2981f815334e1a9ccd
Isaac Yanez <isaac231467@gmail.com>
2022-12-27 15:38:39 -0600

    added another libary so I can ask the user If I can track their location

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,8 +1,22 @@
 import React from "react";
 import { Text, StyleSheet } from "react-native";
-import MapView from "react-native-maps";
+import MapView, { Polyline } from "react-native-maps";
 
 const Map = () => {
+  let tempArray = [];
+  for (let i = 0; i < 20; i++) {
+    if (i % 2 === 0) {
+      tempArray.push({
+        latitude: 37.33233 + i * 0.001,
+        longitude: -122.03121 + i * 0.001,
+      });
+    } else {
+      tempArray.push({
+        latitude: 37.33233 - i * 0.002,
+        longitude: -122.03121 + i * 0.001,
+      });
+    }
+  }
   return (
     <MapView
       initialRegion={{
@@ -12,7 +26,9 @@ const Map = () => {
         latitudeDelta: 0.01,
       }}
       style={styles.map}
-    />
+    >
+      <Polyline coordinates={tempArray} />
+    </MapView>
   );
 };
 
--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { StyleSheet, SafeAreaView } from "react-native";
 import { Text } from "react-native-elements";
 import Map from "../components/Map";
+import { requestForegroundPermissionsAsync } from "expo-location";
 
 const TrackCreateScreen = () => {
+  const [err, setErr] = useState(null);
+  const startWatching = async () => {
+    try {
+      await requestForegroundPermissionsAsync();
+    } catch (error) {
+      setErr(error);
+    }
+  };
+  useEffect(() => {
+    startWatching();
+  }, []);
+
   return (
     <SafeAreaView>
       <Text h2>Create A Track</Text>
       <Map />
+      {err ? <Text>Please enable location services </Text> : null}
     </SafeAreaView>
   );
 };
